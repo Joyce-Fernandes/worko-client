28688f7d1785d62cd9a19623518ba53471a130e1
Leire <leirecalvortega@gmail.com>
2022-03-28 12:01:17 +0200

    index-segundo-cambio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +10,22 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.onWindowScroll();
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+      const scrollOffset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      const header = document.querySelector('header');
+      if(window.onload && scrollOffset === 0){
+        header?.classList.add('header-load');
+        header?.classList.remove('header-top', 'header-scroll');
+      }else if(scrollOffset >= 50){
+        header?.classList.add('header-scroll');
+        header?.classList.remove('header-top', 'header-load');
+      }else if(scrollOffset === 0){
+        header?.classList.add('header-top');
+        header?.classList.remove('header-scroll','header-load');
+      }
+  }
 }
